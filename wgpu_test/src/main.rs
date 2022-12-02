485391d11eb08564c245ce37e9dfe3e33f751c64
Zac Waite <129032699zw@gmail.com>
2022-12-01 23:33:49 -0500

    Implement async state functions with wpgu test

--- wgpu_test/src/main.rs
+++ wgpu_test/src/main.rs
@@ -1,4 +1,8 @@
-use winit::{window::Window, event::WindowEvent};
+use winit::{
+    event::{WindowEvent, Event, KeyboardInput, ElementState, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window,WindowBuilder},
+};
 
 struct State {
     surface: wgpu::Surface,
@@ -36,19 +40,39 @@ impl State {
             None, // Trace path
         ).await.unwrap();
 
-        todo!()
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_supported_formats(&adapter)[0],
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        todo!()
+        false
     }
 
     fn update(&mut self) {
-        todo!()
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
@@ -56,7 +80,46 @@ impl State {
     }
 }
 
+pub async fn run() {
+    // Window setup...
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(&window).await;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size is &&mut so we have to dereference it twice
+                    state.resize(**new_inner_size);
+                }
+                _ => {}
+            }
+        },
+        _ => {}
+    });
+}
+
+
 fn main() {
     println!("Hello, world!");
-    // let state = State::new(
+    pollster::block_on(run());
 }
